ed80b1d3bc68b03eb6612428504cac22d0b3b4ae
GuilFranca <guilfranca27@gmail.com>
2025-04-29 13:54:16 -0300

    aula 29/04

---
+++ Java/GuilhermeFranca/src/main/java/com/mycompany/guilhermefranca/Bhaskara.java
@@ -0,0 +1,27 @@
+package com.mycompany.guilhermefranca;
+
+public class Bhaskara {
+
+    static void bhaskara (double a, double b, double c) {
+        
+                    System.out.println("\n\nScript - Bhaskara");
+
+                    double delta = Math.pow(b, 2) - 4 * a * c;
+
+                    if (delta < 0) {
+                        System.out.println("Delta negativo! Não existem raízes reais.");
+                    } else {
+                        double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                        double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+                        String resultado = String.format("Delta: %.2f\nx1: %.2f\nx2: %.2f", delta, x1, x2);
+                        System.out.println(resultado);
+                    }
+    }
+    
+    public static void main(String[] args) {
+        
+        bhaskara(1,1,-6);
+    
+    }
+    
+}
---
+++ Java/GuilhermeFranca/src/main/java/com/mycompany/guilhermefranca/VoidSoma.java
@@ -0,0 +1,14 @@
+package com.mycompany.guilhermefranca;
+
+public class VoidSoma {
+        
+        static void soma (int a, int b) {
+            int s = a + b;
+            System.out.println(s);
+    }
+        
+        public static void main(String[] args) {
+            soma(5,3);
+    }
+
+}
