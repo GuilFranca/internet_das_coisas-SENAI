7fab93f642cbb251452e6e04828c1f67c2de7574
Guilherme França <guilfranca27@gmail.com>
2025-05-27 11:44:55 -0300

    CRUD biblioteca -  Update

--- Java/ProjetoBiblio/src/projetobiblio/Biblioteca.java
+++ Java/ProjetoBiblio/src/projetobiblio/Biblioteca.java
@@ -1,12 +1,82 @@
 package projetobiblio;
 
+import java.util.List;
 import java.sql.SQLException;
+import java.sql.Connection;
 
 public class Biblioteca {
 
     public static void main(String[] args) {
         
         UsuarioDAO usuarioDao = new UsuarioDAO();
+        
+        Connection connection = null;
+        
+        try {
+       
+            connection = new ConnectionFactory().conectaBD();
+        
+            UsuarioDAO dao = new UsuarioDAO();
+            
+            Usuario usuarioAtualizado = new Usuario();
+            usuarioAtualizado.setId(1);
+            usuarioAtualizado.setNome("Novo nome");
+            usuarioAtualizado.setEmail("novoEmail@gmail.com");
+            usuarioAtualizado.setTelefone("99999-9999");
+            usuarioAtualizado.setTipo_usuario("Administrador");
+            
+            dao.atualizarUsuario(usuarioAtualizado);
+            
+        } catch (SQLException e) {
+            System.out.println("ERRO na operação: " + e.getMessage());
+        } finally {
+            try {
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                System.out.println("ERRO ao fechar onexão: " + e.getMessage());
+            } 
+        }
+        
+        try {
+            
+            int idBuscado = 1;
+            Usuario usuario = usuarioDao.buscarUsurioPorId(idBuscado);
+            
+            if (usuario != null) {
+                
+                System.out.println("Usuário encontrado: ");
+                System.out.println("ID: " + usuario.getId());
+                System.out.println("Nome: " + usuario.getNome());
+                System.out.println("Email: " + usuario.getEmail());
+                System.out.println("Telefone: " + usuario.getTelefone());
+                System.out.println("Tipo: " + usuario.getTipo_usuario());
+                
+            } else {
+                System.out.println("Usuário com ID " + idBuscado + " não encontrado");
+            }
+            
+        } catch (SQLException e) {
+            System.out.println("ERRO ao buscar usuário: " + e.getMessage());
+        }
+        
+        try {
+            
+            List<Usuario> usuarios = usuarioDao.listarUsuarios();
+            if(usuarios.isEmpty()) {
+                System.out.println("Nenhum usuários cadastrado.");
+            } else {
+                for (Usuario usuario: usuarios) {
+                    System.out.println("ID: " + usuario.getId());
+                    System.out.println("Nome: " + usuario.getNome());
+                    System.out.println("Email: " + usuario.getEmail());
+                    System.out.println("Telefone: " + usuario.getTelefone());
+                    System.out.println("Tipo: " + usuario.getTipo_usuario());
+                    System.out.println("--------------------------------------");
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("ERRO ao listar usuários: " + e.getMessage());
+        }
         
 //        Usuario usuario = new Usuario(0, "Jubileu", "jubileu@gmail.com", "61-9976427772", "ADM");;
         
--- Java/ProjetoBiblio/src/projetobiblio/UsuarioDAO.java
+++ Java/ProjetoBiblio/src/projetobiblio/UsuarioDAO.java
@@ -41,11 +41,104 @@ public class UsuarioDAO {
     
     public List<Usuario> listarUsuarios() throws SQLException {
         
+        List<Usuario> lista = new ArrayList<>();
         String sql = "SELECT * FROM usuarios";
         
         PreparedStatement stmt = null;
         ResultSet rs = null;
-        List<Usuario> usuarios = new ArrayList<>();
+        
+        try {
+
+            stmt = connection.prepareStatement(sql);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Usuario usuario = new Usuario (0, "", "", "", "");
+                usuario.setId(rs.getInt("id"));
+                usuario.setNome(rs.getString("nome"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setTelefone(rs.getString("telefone"));
+                usuario.setTipo_usuario(rs.getString("tipo_usuario"));
+                
+                lista.add(usuario);
+            }
+            
+        } catch (SQLException e){
+            System.out.println("Erro ao listar usuários: " + e.getMessage());
+        } finally {
+            if (rs != null) rs.close();
+            if (stmt != null) stmt.close();
+        }
+        
+        return lista;
+        
+    }
+    
+    public Usuario buscarUsurioPorId(int id) throws SQLException {
+        
+        String sql = "SELECT * FROM usuarios WHERE id = ?";
+        
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Usuario usuario = null;
+        
+        try {
+            
+            stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, id);
+            rs = stmt.executeQuery();
+            
+            if (rs.next()) {
+                
+                usuario = new Usuario();
+                usuario.setId(rs.getInt("id"));
+                usuario.setNome(rs.getString("nome"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setTelefone(rs.getString("telefone"));
+                usuario.setTipo_usuario(rs.getString("tipo_usuario"));
+            
+            }
+            
+        } catch (SQLException e) {
+            System.out.println("ERRO ao buscar usuário: " + e.getMessage());
+        } finally {
+            if (rs != null) rs.close();
+            if (stmt != null) stmt.close();
+        }
+        
+        return usuario;
+        
+    }
+    
+    public void atualizarUsuario(Usuario usuario) throws SQLException {
+        
+        String sql = "UPDATE usuarios SET nome = ?, email = ?, telefone = ?, tipo_usuario = ? WHERE id = ?";
+        
+        PreparedStatement stmt = null;
+        
+        try {
+            
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, usuario.getNome());
+            stmt.setString(2, usuario.getEmail());
+            stmt.setString(3, usuario.getTelefone());
+            stmt.setString(4, usuario.getTipo_usuario());
+            stmt.setInt(5, usuario.getId());
+            
+//            stmt.executeUpdate();
+            int linhasAfetadas = stmt.executeUpdate();
+            
+            if (linhasAfetadas > 0) {
+                System.out.println("Usuário atualizado com sucesso!");
+            } else {
+                System.out.println("Nenhum usuário encontrado com o ID informado.");
+            }
+            
+        } catch (SQLException e) {
+            System.out.println("ERRO ao atualizar usuário: " + e.getMessage());
+        } finally {
+            if (stmt != null) stmt.close();
+        }
         
     }
     
